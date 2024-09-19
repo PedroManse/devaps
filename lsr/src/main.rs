8f73d34531ef1966912653dc911abe8e8ee40d2c
Pedro Manse <pedro.manse@dmk3.com.br>
2024-09-19 11:41:42 -0300

    lsr: ignore error on broken pipe (in case of | head etc)

--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,17 +1,27 @@
-type Result = std::result::Result<(), std::io::Error>;
+use std::io::{self, Write};
+type Result<T> = std::result::Result<T, io::Error>;
 
-fn main() -> Result {
+fn main() -> Result<()> {
+    let mut stdout = io::stdout().lock();
     let mut dirs = 0;
     let args = std::env::args();
     args.skip(1).into_iter().try_fold((), |_, arg|{
         dirs+=1;
-        println!("{}", lsr::read_dir(arg.as_ref())?);
-        Result::Ok(())
+        let st = lsr::read_dir(arg.as_ref())?;
+        write!(stdout, "{}", st).inspect_err(die_on_pipe)
     })?;
-    println!("{dirs}");
     if dirs==0 {
-        println!("{}", lsr::read_dir(".".as_ref())?);
+        let st = lsr::read_dir(".".as_ref())?;
+        write!(stdout, "{}", st).inspect_err(die_on_pipe)?;
     }
     Ok(())
+}
+
+// in case piped to head, don't eprint on closed pipe
+fn die_on_pipe(r: &io::Error)  {
+    match r.kind() {
+        io::ErrorKind::BrokenPipe=>std::process::exit(0),
+        _=>(),
+    };
 }
 
