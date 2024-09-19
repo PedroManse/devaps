e127138217f192944015f4a053d235bb3a53f289
Pedro Manse <pedro.manse@dmk3.com.br>
2024-09-19 13:17:16 -0300

    filte2: don't panic on broken pipe

--- filte2-rs/src/main.rs
+++ filte2-rs/src/main.rs
@@ -1,5 +1,5 @@
 use filte2_rs::*;
-use std::io::{stdin, BufRead};
+use std::io::{self, Write, BufRead};
 
 const HELP: &'static str = "
 usage filter [i](mode)(pattern)
@@ -28,17 +28,27 @@ fn help() -> ! {
 }
 
 fn main() -> eyre::Result<()> {
+    let mut stdout = io::stdout().lock();
     let args: Vec<_> = std::env::args().skip(1).collect();
     if args.len() == 0 {
         help();
     }
     let filters = reader::parse(args.into_iter())?;
 
-    for line in stdin().lock().lines() {
+    for line in io::stdin().lock().lines() {
         let line = line?;
         if filters.compare(&line) {
-            println!("{line}");
+            stdout.write_all(line.as_bytes()).inspect_err(die_on_pipe)?;
         }
     }
     Ok(())
 }
+
+// in case piped to head, don't eprint on closed pipe
+fn die_on_pipe(r: &io::Error)  {
+    match r.kind() {
+        io::ErrorKind::BrokenPipe=>std::process::exit(0),
+        _=>(),
+    };
+}
+
