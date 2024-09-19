8cf8fb64099f66c2ec7786da49433ede5a6ba41e
Pedro Manse <pedro.manse@dmk3.com.br>
2024-09-19 10:39:46 -0300

    lsr: if 0 args print lsr of .

--- lsr/src/main.rs
+++ lsr/src/main.rs
@@ -1,11 +1,17 @@
-use std::io;
+type Result = std::result::Result<(), std::io::Error>;
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result {
+    let mut dirs = 0;
     let args = std::env::args();
     args.skip(1).into_iter().try_fold((), |_, arg|{
-        let nodes = lsr::read_dir(arg.as_ref())?;
-        println!("{nodes}");
-        Ok::<(), io::Error>(())
-    })
+        dirs+=1;
+        println!("{}", lsr::read_dir(arg.as_ref())?);
+        Result::Ok(())
+    })?;
+    println!("{dirs}");
+    if dirs==0 {
+        println!("{}", lsr::read_dir(".".as_ref())?);
+    }
+    Ok(())
 }
 
