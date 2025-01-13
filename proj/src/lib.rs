87b22c66731e941a79e277fe2fc91a83ff4b6817
Pedro Manse <pedro.manse@dmk3.com.br>
2025-01-13 15:27:10 -0300

    proj: add nix shell option (other is named posix)

--- proj/src/lib.rs
+++ proj/src/lib.rs
@@ -1,5 +1,19 @@
+use std::collections::hash_map::Entry;
 pub use std::collections::HashMap;
+use serde::de::{Deserialize, Deserializer};
 use std::ffi::CString;
+
+fn lowercase_map<'de, D>(deserializer: D) -> Result<HashMap<String, Project>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let mut low = HashMap::new();
+    let x: HashMap<String, Project> = Deserialize::deserialize(deserializer)?;
+    for (k, v) in x {
+        low.insert(k.to_lowercase(), v);
+    }
+    Ok(low)
+}
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -13,34 +27,103 @@ pub enum Error {
     IOError(#[from] std::io::Error),
     #[error(transparent)]
     NixErr(#[from] nix::errno::Errno),
-    #[error(transparent)]
-    TomlSerializeError(#[from] toml::ser::Error),
+    //#[error(transparent)]
+    //TomlSerializeError(#[from] toml::ser::Error),
     #[error(transparent)]
     TomlDeserializeError(#[from] toml::de::Error),
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-pub struct Config(pub HashMap<String, Project>);
+#[derive(serde::Deserialize, Debug, Clone)]
+pub struct CaseinsString(String);
 
-#[derive(serde::Serialize, serde::Deserialize, Clone, Debug)]
-pub struct Project {
-    pub shell: CString,
-    pub shell_args: Vec<CString>,
-    pub vars: HashMap<String, String>,
-    pub dir: String,
+impl PartialEq for CaseinsString {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.to_lowercase().eq(&other.0.to_lowercase())
+    }
 }
+
+impl std::hash::Hash for CaseinsString {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.to_lowercase().hash(state)
+    }
+}
+
+impl Eq for CaseinsString { }
+
+// TODO: make String newtype where impl Hash, Eq ignores case
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct Config(
+    //#[serde(deserialize_with = "lowercase_map")]
+    pub HashMap<CaseinsString, Project>
+);
 
 impl Config {
     pub fn execute(mut self, name: String) -> Result<(), Error> {
-        match self.0.remove(&name) {
+        match self.0.remove(&CaseinsString(name.clone())) {
             None => Err(Error::UnknownProject(name)),
             Some(proj) => proj.execute(name)
         }
     }
 }
 
+#[derive(serde::Deserialize, Clone, Debug)]
+pub enum Project {
+    Posix(Posix),
+    Nix(Nix),
+}
+
 impl Project {
-    pub fn execute(&self, name: String) -> Result<(), Error> {
+    pub fn execute(self, name: String) -> Result<(), Error> {
+        match self {
+            Project::Nix(n) => n.execute(name),
+            Project::Posix(n) => n.execute(name),
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct Nix {
+    pub shell: CString,
+    pub packages: Vec<CString>,
+    pub vars: HashMap<String, String>,
+    pub dir: String,
+    pub config_file: Option<CString>, //TODO
+}
+
+impl Nix {
+    pub fn build_args(shell: CString, deps: Vec<CString>) -> Vec<CString> {
+        [shell, c"--packages".to_owned()].into_iter().chain(deps).collect()
+    }
+    pub fn execute(self, name: String) -> Result<(), Error> {
+        nix::unistd::chdir(&std::path::PathBuf::from(&self.dir))?;
+        let vars: Vec<_> = std::env::vars()
+            .into_iter()
+            .chain(self.vars)
+            .chain([("COMPUTER_NAME".to_owned(), name)])
+            .map(|(k, v)| format!("{k}={v}"))
+            .map(std::ffi::CString::new)
+            .map(Result::unwrap)
+            .collect();
+
+        nix::unistd::execvpe(
+            c"nix-shell",
+            &Nix::build_args(self.shell, self.packages),
+            &vars
+        )?;
+        Ok(())
+    }
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct Posix {
+    pub shell: CString,
+    pub shell_args: Vec<CString>,
+    pub vars: HashMap<String, String>,
+    pub dir: String,
+}
+
+impl Posix {
+    pub fn execute(self, name: String) -> Result<(), Error> {
         nix::unistd::chdir(&std::path::PathBuf::from(&self.dir))?;
         let vars: Vec<_> = std::env::vars()
             .into_iter()
