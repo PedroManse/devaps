e90e4811119e91b72d0b859b52cbebdfb100ffbc
Pedro Manse <pedro.manse@dmk3.com.br>
2025-02-13 12:42:49 -0300

    todo-rs lib.rs: count and report todos

--- todo-rs/src/lib.rs
+++ todo-rs/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub mod conf;
@@ -9,7 +10,6 @@ const LOCAL_CONF_NAME: &'static str = "todo";
 pub const CONF_LEAF: &'static str = const_format::formatcp!("{CONF_NAME}.{CONF_EXT}");
 pub const LOCAL_CONF_LEAF: &'static str = const_format::formatcp!(".{LOCAL_CONF_NAME}.{CONF_EXT}");
 
-
 #[derive(thiserror::Error, Debug)]
 pub enum TDError {
     #[error(transparent)]
@@ -18,28 +18,51 @@ pub enum TDError {
     TomlDeError(#[from] toml::de::Error),
     #[error(transparent)]
     IOError(#[from] std::io::Error),
-    #[error("")]
+    #[error("Config file not found")]
     ConfigNotFound,
 }
 
 pub fn rev_find_config() -> Option<PathBuf> {
     let mut cwd = std::env::current_dir().unwrap();
     loop {
-        let Some(next) = cwd.parent() else {
-            break None
-        };
+        let Some(next) = cwd.parent() else { break None };
         let conf = cwd.clone().join(LOCAL_CONF_LEAF);
         if conf.exists() {
-            break Some(conf)
+            break Some(conf);
         }
         cwd = next.to_path_buf();
     }
 }
 
 pub fn find_global_config() -> Option<PathBuf> {
-    std::env::var("TODORS_CONFIG").ok().map(PathBuf::from).or(
-        directories::ProjectDirs::from("", "", "todo-rs").map(|d|d.config_dir().to_path_buf().join(CONF_LEAF))
-    )
+    std::env::var("TODORS_CONFIG")
+        .ok()
+        .map(PathBuf::from)
+        .or(directories::ProjectDirs::from("", "", "todo-rs")
+            .map(|d| d.config_dir().to_path_buf().join(CONF_LEAF)))
 }
 
+impl crate::filstu::FPath {
+    pub fn count_todos(&self, cfg: &crate::conf::Config) -> Result<usize, TDError> {
+        let finders = cfg.get_todo_finders(self.0.as_path());
+        let cont = std::fs::read_to_string(&self.0)?;
+        Ok(finders
+            .into_iter()
+            .fold(0, |i, f| i + f.find_iter(&cont).count()))
+    }
 
+    pub fn report_todos(&self, cfg: &crate::conf::Config) -> Result<HashMap<usize, String>, TDError> {
+        let finders = cfg.get_todo_finders(self.0.as_path());
+        let cont = std::fs::read_to_string(&self.0)?;
+        let mut matches = HashMap::new();
+        for finder in finders {
+            for mt in finder.find_iter(&cont) {
+                let txt = mt.as_str().to_string();
+                let start = mt.start();
+                matches.insert(start, txt);
+            }
+        }
+
+        Ok(matches)
+    }
+}
