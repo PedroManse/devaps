bf98d1ce25425450c043d53585ee8e5189c5d760
Manse <pedro.manse@dmk3.com.br>
2025-07-25 15:05:32 -0300

    gs2: cargo fmt

--- gs2-rs/src/main.rs
+++ gs2-rs/src/main.rs
@@ -3,7 +3,7 @@ use gs2::*;
 fn main() {
     let repo = match git2::Repository::discover(".") {
         Ok(repo) => repo,
-        Err(_) => return
+        Err(_) => return,
     };
     if repo.is_bare() {
         print!(" \x1b[1;4;35m<bare>\x1b[0m ");
@@ -12,22 +12,21 @@ fn main() {
 
     // get branch name, remote
     let mut status_reporter = match StatusReport::new(&repo) {
-        Ok(s)=>s,
-        Err(e)=>{
+        Ok(s) => s,
+        Err(e) => {
             eprintln!("{e}");
-            return
+            return;
         }
     };
     // get diffs
     if let Err(e) = status_reporter.update_statuses() {
         eprintln!("{e}");
-        return
+        return;
     };
     // get remote graph
     if let Err(e) = status_reporter.update_graph() {
         eprintln!("{e}");
-        return
+        return;
     };
     print!("{status_reporter}");
 }
-
